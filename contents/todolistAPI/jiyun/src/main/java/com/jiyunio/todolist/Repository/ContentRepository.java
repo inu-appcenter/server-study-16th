e32ae1c4d1b15decc34ae091741046d27f187ecc
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2024-04-13 20:08:53 +0900

    Delete contents/todolistAPI/jiyun directory

--- contents/todolistAPI/jiyun/src/main/java/com/jiyunio/todolist/Repository/ContentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jiyunio.todolist.Repository;
-
-import com.jiyunio.todolist.Domain.Content;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContentRepository extends JpaRepository<Content, Long> {
-}
--- contents/todolistAPI/jiyun/src/main/java/com/jiyunio/todolist/Repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jiyunio.todolist.Repository;
-
-import com.jiyunio.todolist.Domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
