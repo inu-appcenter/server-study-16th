0f2b529b4fdc8662e366303dd6f0e88e5d3d5512
ImKEISS <kss5122@naver.com>
2024-05-15 14:43:10 +0900

    [Feat] 누락된 Swagger API 명세 추가 #32

--- contents/todoListAPI/seungseop/todolist/src/main/java/com/serverstudy/todolist/controller/UserController.java
+++ contents/todoListAPI/seungseop/todolist/src/main/java/com/serverstudy/todolist/controller/UserController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -22,6 +23,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User", description = "User API 입니다.")
 @Validated
 @RestController
 @RequestMapping("/api/user")
